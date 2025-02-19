5b13a0e7e5e2e4b8f4e907afc7b881f4db613eb7
Carla Clemente <carlaclefig@gmail.com>
2025-02-19 16:10:03 -0500

    feat: adds initial conditions function for the game

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let friendsList = [];
 
 function assignClassText(clase, text) {
-  let classHTML = document.getElementsByClassName(clase);
+  let classHTML = document.querySelector(clase);
   classHTML.innerHTML = text;
   return;
 }
+
+function initialConditions() {
+  assignClassText(".name-list", "Lista de amigos");
+  assignClassText(".result-list", "El amigo secreto es ...");
+}
+initialConditions();
