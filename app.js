bbb09d6ae36ef68184577fa2e6a81ed9e8f6494e
Carla Clemente <carlaclefig@gmail.com>
2025-02-19 18:21:13 -0500

    feat: added the update friends list and clean box functions, to be used in addFriend()

--- app.js
+++ app.js
@@ -12,20 +12,43 @@ function initialConditions() {
 }
 initialConditions();
 
+function updateList() {
+  let listCollection = document.getElementsByClassName("name-list");
+  let list = listCollection[0];
+  list.innerHTML = "";
+
+  friendsList.forEach((friendName) => {
+    let li = document.createElement("li");
+    li.textContent = friendName;
+    list.appendChild(li);
+  });
+}
+
+function cleanBox() {
+  let inputNameCollection = document.getElementsByClassName("input-name");
+  inputNameCollection[0].value = "";
+}
+
 function addFriend() {
   let inputNameCollection = document.getElementsByClassName("input-name");
 
   let friend = inputNameCollection[0].value.trim();
+  let orientation = inputNameCollection[0];
 
   for (let i = 0; i < friendsList.length; i++) {
     if (friendsList[i] == friend) {
       alert("Nombre de amigo ya ingresado");
+      cleanBox();
+      orientation.focus();
       return;
     }
   }
 
   if (friend != "") {
     friendsList.push(friend);
+    updateList();
+    cleanBox();
+    orientation.focus();
   }
 
   console.log(friendsList);
